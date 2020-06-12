bdaf39cf3db50c873d2d331995b98ba3298a9fb7
Louis Quinnell <_@ziorobo.co>
2020-06-13 00:39:22 +1000

    Use ? (equivalent, but more concise)

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,7 @@ struct Cli {
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Cli::from_args();
-    let result = std::fs::read_to_string(&args.path);
-
-    let content = match result {
-        Ok(content) => content,
-        Err(error) => {
-            return Err(error.into());
-        }
-    };
+    let content = std::fs::read_to_string(&args.path)?;
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
