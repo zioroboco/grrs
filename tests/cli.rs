44ea8df6238a9f000703352b5ca4ac1d92ebd60f
Louis Quinnell <_@ziorobo.co>
2020-06-13 19:08:35 +1000

    Add integration test matching patterns in temp files

--- tests/cli.rs
+++ tests/cli.rs
@@ -1,7 +1,9 @@
 use assert_cmd::prelude::*;
 use predicates::prelude::*;
 use std::error::Error;
+use std::io::Write;
 use std::process::Command;
+use tempfile::NamedTempFile;
 
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn Error>> {
@@ -11,6 +13,21 @@ fn file_doesnt_exist() -> Result<(), Box<dyn Error>> {
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("No such file or directory"));
+
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn Error>> {
+    let mut cmd = Command::cargo_bin("grrs")?;
+
+    let mut file = NamedTempFile::new()?;
+    writeln!(file, "one\ntwo\nthree\nfour\n")?;
+
+    cmd.arg("t").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("two\nthree"));
 
     Ok(())
 }
